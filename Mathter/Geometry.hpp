f8fef29804b09d90d0497dcc41f1297c8699d8c1
petiaccja <kardospeter1994@erad.com>
2017-04-21 18:17:44 +0200

    added plane-line intersections

---
+++ Mathter/Geometry.hpp
@@ -0,0 +1,226 @@
+#pragma once
+
+
+#include "Vector.hpp"
+
+
+namespace mathter {
+
+
+//------------------------------------------------------------------------------
+// Shapes
+//------------------------------------------------------------------------------
+
+template <class T, int Dim>
+class Hyperplane;
+
+template <class T, int Dim>
+class Line {
+	using VectorT = Vector<T, Dim>;
+public:
+	Line() : direction(0), base(0) {
+		direction(0) = 1;
+	}
+	Line(const VectorT& base, const VectorT& direction) : direction(direction), base(base) {
+		assert(impl::AlmostEqual(direction.Length(), T(1)));
+	}
+	Line(const VectorT& point1, const VectorT point2) {
+		direction = (point2 - point1).Normalized();
+		base = point1;
+	}
+	template <typename std::enable_if<Dim == 2, int>::type = 0>
+	Line(const Hyperplane<T, 2>& plane)
+
+		VectorT Direction() const { return direction; }
+	VectorT Base() const { return base; }
+	VectorT PointAt(T param) const { return base + param*direction; }
+private:
+	VectorT direction, base;
+};
+
+
+
+template <class T, int Dim>
+class LineSegment {
+	using VectorT = Vector<T, Dim>;
+public:
+	LineSegment() : point1(0), point2(0) {
+		point2(0) = 1;
+	}
+	LineSegment(const VectorT& base, const VectorT& direction, T length) {
+		point1 = base;
+		point2 = base + direction*length;
+	}
+	LineSegment(const VectorT& point1, const VectorT& point2) : point1(point1), point2(point2) {}
+
+	T Length() const { return (point2 - point1).Length(); }
+	VectorT Direction() const { return (point2 - point1).Normalized(); }
+	VectorT Start() const { return point1; }
+	VectorT End() const { return point2; }
+	VectorT Interpol(T t) const { return t*point2 + (T(1) - t)*point1; }
+
+	Line<T, Dim> Line() const {
+		return Line<T, Dim>(point1, Direction());
+	}
+private:
+	VectorT point1, point2;
+};
+
+
+
+template <class T, int Dim>
+class Hyperplane {
+	using VectorT = Vector<T, Dim>;
+public:
+	Hyperplane() : normal(0), scalar(0) { normal(0) = 1; }
+	Hyperplane(const VectorT& normal, const VectorT& base) : normal(normal) {
+		assert(impl::AlmostEqual(normal.Length(), T(1)));
+		scalar = VectorT::Dot(normal, base);
+	}
+	Hyperplane(const VectorT& normal, T scalar) : normal(normal), scalar(scalar) {
+		assert(impl::AlmostEqual(normal.Length(), T(1)));
+	}
+
+	const VectorT& Normal() const { return normal; }
+	T Scalar() const { return scalar; }
+private:
+	VectorT normal;
+	T scalar;
+};
+
+
+template <class T, int Dim>
+template <typename std::enable_if<Dim == 2, int>::type>
+Line<T, Dim>::Line(const Hyperplane<T, 2>& plane) {
+	// Intersect plane's line with line through origo perpendicular to plane to find suitable base
+	T a = plane.Normal()(0);
+	T b = plane.Normal()(1);
+	T div = -(a*a + b*b);
+	base = {a*d/div, b*d/div};
+	direction = { -b, a };
+}
+
+
+//------------------------------------------------------------------------------
+// Intersections
+//------------------------------------------------------------------------------
+
+
+template <class T, class U>
+class Intersection;
+
+
+
+template <class T, class U>
+auto Intersect(const T& t, const U& u) {
+	return Intersection<T, U>(t, u);
+}
+
+
+// Plane-line intersection
+template <class T, int Dim>
+class Intersection<Hyperplane<T, Dim>, Line<T, Dim>> {
+	using PlaneT = Hyperplane<T, Dim>;
+	using LineT = Line<T, Dim>;
+	using VectorT = Vector<T, Dim>;
+public:
+	Intersection(const PlaneT& plane, const LineT& line);
+
+	bool Intersecting() const { return !isinf(param); }
+	VectorT Point() const { return line.PointAt(param); }
+	T LineParameter() const { return param; }
+private:
+	LineT line;
+	T param;
+};
+
+template <class T, int Dim>
+class Intersection<Line<T, Dim>, Hyperplane<T, Dim>> : public Intersection<Hyperplane<T, Dim>, Line<T, Dim>> {
+public:
+	Intersection(const LineT& line, const PlaneT& plane) : Intersection<Hyperplane<T, Dim>, Line<T, Dim>>(plane, line) {}
+};
+
+
+
+// Plane-line segment intersection
+template <class T, int Dim>
+class Intersection<Hyperplane<T, Dim>, LineSegment<T, Dim>> {
+	using PlaneT = Hyperplane<T, Dim>;
+	using LineT = LineSegment<T, Dim>;
+	using VectorT = Vector<T, Dim>;
+public:
+	Intersection(const PlaneT& plane, const LineT& line) {
+		lineSegment = line;
+		auto intersection = Intersect(plane, line.Line());
+		param = intersection.LineParameter() / line.Length();
+	}
+
+	bool Intersecting() const { T(0) <= param && param <= T(1); }
+	VectorT Point() const { return lineSegment.Interpol(param); }
+	T InterpolParameter() const { return param; }
+	T LineParameter() const { return param * line.Length(); }
+private:
+	LineT lineSegment;
+	T param;
+};
+
+template <class T, int Dim>
+class Intersection<LineSegment<T, Dim>, Hyperplane<T, Dim>> : public Intersection<Hyperplane<T, Dim>, LineSegment<T, Dim>> {
+public:
+	Intersection(const LineT& line, const PlaneT& plane) : Intersection<Hyperplane<T, Dim>, LineSegment<T, Dim>>(plane, line) {}
+};
+
+
+
+template <class T, int Dim>
+Intersection<Hyperplane<T, Dim>, Line<T, Dim>>::Intersection(const PlaneT& plane, const LineT& line) {
+	this->line = line;
+
+	// We have to solve the system of linear equations for x,y,z,t
+	//	|d |	|a b c 0|	|x|
+	//	|px|	|1 0 0 p|	|y|
+	//	|py|  =	|0 1 0 q| *	|z|
+	//	|pz|	|0 0 1 r|	|t|
+	//   b    =     A     *  x
+	// where [px,py,pz] + t*[p,q,r] = [x,y,z] is the line's equation
+	// and ax + by + cz + d = 0 is the plane's equation
+
+	Vector<T, Dim + 1> b;
+	Matrix<T, Dim + 1, Dim + 1> A_inv;
+	Vector<T, Dim + 1> A_inv_t;
+	Vector<T, Dim + 1> x;
+
+	// Fill up 'b'
+	b = plane.Scalar() | line.Base();
+
+	// Fill up 'A_inv_t', which is the last line of A^-1, used to calculate 't'
+	A_inv_t = -1 | plane.Normal();
+
+	// Compute result of the equation
+	T scaler = VectorT::Dot(line.Direction(), plane.Normal());
+	T x_t = VectorT::Dot(A_inv_t, b);
+	T t = x_t / scaler;
+	param = t;
+}
+
+
+
+// 2D line intersection
+template <class T>
+class Intersection<Line<T, 2>, Line<T, 2>> {
+	using LineT = Line<T, 2>;
+public:
+	Intersection(const LineT&, const LineT&);
+
+	bool Intersecting() const;
+	T LineParameter1() const;
+	T LineParameter2() const;
+	Vector<T, 2> Point() const;
+private:
+	T param1, param2;
+};
+
+
+
+
+} // namespace mathter
