2a935daa7180a480141bf864654bb7cc3f73d108
petiaccja <kardospeter1994@erad.com>
2017-05-23 16:57:24 +0200

    started writing quaternions

--- Mathter/Quaternion.hpp
+++ Mathter/Quaternion.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 
+#include "Vector.hpp"
+
+
 namespace mathter {
 
 
@@ -8,10 +11,150 @@ namespace mathter {
 template <class T>
 class Quaternion {
 public:
-	
+	union {
+		struct { T s, i, j, k };
+		struct { T w, x, y, z };
+		Vector<T, 4, false> vec;
+	};
+
+	//-----------------------------------------------
+	// Constructors
+	//-----------------------------------------------
+	Quaternion() = default;
+	Quaternion(T scalar, T x, T y, T z) : s(scalar), x(x), y(y), z(z) {}
+	Quaternion(T scalar, const Vector<T, 3, true>& vector) : s(scalar), x(vector.x), y(vector.y), z(vector.z) {}
+	Quaternion(T scalar, const Vector<T, 3, false>& vector) : s(scalar), x(vectorv.x), y(vector.y), z(vector.z) {}
+	explicit Quaternion(const Vector<T, 3, true>& vector) : Quaternion(0, vector) {}
+protected:
+	explicit Quaternion(const Vector<T, 4, false>& vec) : vec(vec) {}
+public:
 
 
+	//-----------------------------------------------
+	// Alternate constructions
+	//-----------------------------------------------
+
+	template <bool Packed>
+	static Quaternion AxisAngle(const Vector<T, 3, Packed>& axis, T angle) {
+		angle *= T(0.5);
+		return Quaternion(cos(angle), axis * sin(angle));
+	}
+
+
+	//-----------------------------------------------
+	// Arithmetic
+	//-----------------------------------------------
+	Quaternion& operator+=(const Quaternion& rhs) {
+		vec += rhs.vec;
+		return *this;
+	}
+
+	Quaternion& operator-=(const Quaternion& rhs) {
+		vec -= rhs.vec;
+		return *this;
+	}
+
+	Quaternion& operator*=(const Quaternion& rhs) {
+		T w_, x_, y_, z_;
+		w_ = s*rhs.s + x*rhs.x + y*rhs.y + z*rhs.z;
+		x_ = s*rhs.x + x*rhs.s + y*rhs.z + z*rhs.y;
+		y_ = s*rhs.y + x*rhs.z + y*rhs.s + z*rhs.x;
+		z_ = s*rhs.z + x*rhs.y + y*rhs.x + z*rhs.s;
+		w = w_;
+		x = x_;
+		y = y_;
+		z = z_;
+		return *this;
+	}
+
+	Quaternion& operator*=(T s) {
+		w *= s;
+		x *= s;
+		y *= s;
+		z *= s;
+		return *this;
+	}
+	Quaternion& operator/=(T s) {
+		*this *= T(1) / s;
+		return *this;
+	}
+
+	Quaternion operator+(const Quaternion& rhs) const {
+		return Quaternion(*this) += rhs;
+	}
+	Quaternion operator-(const Quaternion& rhs) const {
+		return Quaternion(*this) -= rhs;
+	}
+	Quaternion operator*(const Quaternion& rhs) const {
+		return Quaternion(*this) *= rhs;
+	}
+	Quaternion operator*(T s) {
+		return Quaternion(*this) * s;
+	}
+	Quaternion operator/(T s) {
+		return Quaternion(*this) / s;
+	}
+
+
+	//-----------------------------------------------
+	// Functions
+	//-----------------------------------------------
+
+	T Length() const {
+		return vec.Length();
+	}
+	T LengthSquared() const {
+		return vec.LengthSquared();
+	}
+
+	Quaternion Normalized() const {
+		return Quaternion(vec.Normalized());
+	}
+	Quaternion& Normalize() {
+		vec.Normalize();
+		return *this;
+	}
+
+	Quaternion& Invert() {
+		x = -x;
+		y = -y;
+		z = -z;
+	}
+	Quaternion Inverse() const {
+		return Quaternion(*this).Invert();
+	}
+
+	//-----------------------------------------------
+	// Matrix conversions
+	//-----------------------------------------------
+	template <class U, eMatrixOrder Order, eMatrixLayout Layout, bool Packed>
+	explicit operator Matrix<U, 3, 3, Order, Layout, Packed>() {
+
+	}
+	template <class U, eMatrixLayout Layout, bool Packed>
+	explicit operator Matrix<U, 3, 4, eMatrixOrder::PRECEDE_VECTOR, Layout, Packed>() {
+
+	}
+	template <class U, eMatrixLayout Layout, bool Packed>
+	explicit operator Matrix<U, 4, 3, eMatrixOrder::FOLLOW_VECTOR, Layout, Packed>() {
+
+	}
+	template <class U, eMatrixOrder Order, eMatrixLayout Layout, bool Packed>
+	explicit operator Matrix<U, 4, 4, Order, Layout, Packed>() {
+
+	}
 };
+
+
+
+template <class T, class U, class = typename std::enable_if<std::is_same<U, Quaternion<T>>::value>::type>
+Quaternion<T> operator*(U s, const Quaternion<T>& rhs) {
+	return Quaternion<T>(*this) * s;
+}
+template <class T, class U, class = typename std::enable_if<std::is_same<U, Quaternion<T>>::value>::type>
+Quaternion<T> operator/(U s, const Quaternion<T>& rhs) {
+	return Quaternion<T>(*this) / s;
+}
 
 
 
--- Measure.cpp
+++ Measure.cpp
@@ -4,6 +4,7 @@
 #include <chrono>
 #include <iostream>
 #include <string>
+#include <numeric>
 
 #include "Mathter/Vector.hpp"
 #include "Mathter/Matrix.hpp"
@@ -14,13 +15,17 @@
 #include <Windows.h>
 #endif
 
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+
 
 using namespace std;
 using namespace mathter;
 
 
 template <class Type, int Rows1, int Columns1, eMatrixLayout Layout1, int Rows2 = Columns1, int Columns2 = Rows1, eMatrixLayout Layout2 = Layout1, bool Packed = false>
-double MeasureMatrixMultiplication() {
+double MeasureMatrixMultiplication(double* cycles = nullptr) {
 	using LeftT = Matrix<Type, Rows1, Columns1, eMatrixOrder::FOLLOW_VECTOR, Layout1, Packed>;
 	using RightT = Matrix<Type, Rows2, Columns2, eMatrixOrder::FOLLOW_VECTOR, Layout2, Packed>;
 	using ResultT = typename decltype(LeftT()*RightT());
@@ -38,24 +43,34 @@ double MeasureMatrixMultiplication() {
 
 	std::chrono::high_resolution_clock::time_point startTime;
 	std::chrono::high_resolution_clock::time_point endTime;
+	int64_t startCycle = 0, endCycle = -1;
 
 
 	startTime = std::chrono::high_resolution_clock::now();
+#ifdef _MSC_VER
+	startCycle = (int64_t)__rdtsc();
+#endif
 	for (int j = 0; j < repeatCount; ++j) {
 		for (int i = 0; i < iterationCount; ++i) {
 			result[i] = left[i] * right[i];
 		}
 	}
+#ifdef _MSC_VER
+	endCycle = (int64_t)__rdtsc();
+#endif
 	endTime = std::chrono::high_resolution_clock::now();
 
 
+	if (cycles) {
+		*cycles = double(endCycle - startCycle) / iterationCount / repeatCount;
+	}
 	double totalTime = chrono::duration_cast<chrono::nanoseconds>(endTime - startTime).count() * 1e-9;
 	return totalTime / iterationCount / repeatCount;
 }
 
 
 template <class Type, int Rows, int Columns, eMatrixLayout Layout, bool Packed = false>
-double MeasureMatrixAddition() {
+double MeasureMatrixAddition(double* cycles = nullptr) {
 	using MatrixT = Matrix<Type, Rows, Columns, eMatrixOrder::FOLLOW_VECTOR, Layout, Packed>;
 
 	constexpr int repeatCount = 50;
@@ -71,17 +86,27 @@ double MeasureMatrixAddition() {
 
 	std::chrono::high_resolution_clock::time_point startTime;
 	std::chrono::high_resolution_clock::time_point endTime;
+	int64_t startCycle = 0, endCycle = -1;
 
 
 	startTime = std::chrono::high_resolution_clock::now();
+#ifdef _MSC_VER
+	startCycle = (int64_t)__rdtsc();
+#endif
 	for (int j = 0; j < repeatCount; ++j) {
 		for (int i = 0; i < iterationCount; ++i) {
 			result[i] = left[i] + right[i];
 		}
 	}
+#ifdef _MSC_VER
+	endCycle = (int64_t)__rdtsc();
+#endif
 	endTime = std::chrono::high_resolution_clock::now();
 
 
+	if (cycles) {
+		*cycles = double(endCycle - startCycle) / iterationCount / repeatCount;
+	}
 	double totalTime = chrono::duration_cast<chrono::nanoseconds>(endTime - startTime).count() * 1e-9;
 	return totalTime / iterationCount / repeatCount;
 }
@@ -96,10 +121,13 @@ void Measure() {
 	BOOL success;
 	cout << "[Initialize]" << endl;
 	success = SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
-	cout << (success ? "Process set to highest priority." : "Failed to set process priority class.");
+	cout << (success ? "Process set to highest priority." : "Failed to set process priority class - times may have jitter.");
 	cout << endl;
 	success = SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
-	cout << (success ? "Thread set to highest priority." : "Failed to set thread priority class.");
+	cout << (success ? "Thread set to highest priority." : "Failed to set thread priority class - times may have jitter.");
+	cout << endl;
+	success = SetThreadAffinityMask(GetCurrentThread(), 1) != 0;
+	cout << (success ? "Thread is limited to 1st CPU core." : "Failed to set thread affinity - cycle count may be incorrect.");
 	cout << endl << endl;
 #endif
 
@@ -107,8 +135,8 @@ void Measure() {
 
 
 	// Multiplication measures
-	std::string mulLabels[7] = {
-		"type", "dim1", "dim2", "layout1", "layout2", "packed", "time/op"
+	std::string mulLabels[8] = {
+		"type", "dim1", "dim2", "layout1", "layout2", "packed", "time/op", "\tcycles"
 	};
 
 	constexpr int mulNumCases = 10;
@@ -124,17 +152,18 @@ void Measure() {
 		{ "float",	"3x4",	"4x3",	"R",	"R",	"F" },
 		{ "float",	"3x4",	"4x3",	"R",	"R",	"T" },
 	};
-	double mulTimes[mulNumCases] = {
-		MeasureMatrixMultiplication<float, 2, 2, ROW, 2, 2, ROW, false>(),
-		MeasureMatrixMultiplication<float, 3, 3, ROW, 3, 3, ROW, false>(),
-		MeasureMatrixMultiplication<float, 4, 4, ROW, 4, 4, ROW, false>(),
-		MeasureMatrixMultiplication<float, 4, 4, ROW, 4, 4, COL, false>(),
-		MeasureMatrixMultiplication<float, 4, 4, COL, 4, 4, ROW, false>(),
-		MeasureMatrixMultiplication<float, 4, 4, COL, 4, 4, COL, false>(),
-		MeasureMatrixMultiplication<double, 4, 4, ROW, 4, 4, ROW, false>(),
-		MeasureMatrixMultiplication<float, 4, 3, ROW, 3, 4, ROW, false>(),
-		MeasureMatrixMultiplication<float, 3, 4, ROW, 4, 3, ROW, false>(),
-		MeasureMatrixMultiplication<float, 3, 4, ROW, 4, 3, ROW, true>(),
+	std::array<double, mulNumCases> mulCycles;
+	std::array<double, mulNumCases> mulTimes = {
+		MeasureMatrixMultiplication<float, 2, 2, ROW, 2, 2, ROW, false>(&mulCycles[0]),
+		MeasureMatrixMultiplication<float, 3, 3, ROW, 3, 3, ROW, false>(&mulCycles[1]),
+		MeasureMatrixMultiplication<float, 4, 4, ROW, 4, 4, ROW, false>(&mulCycles[2]),
+		MeasureMatrixMultiplication<float, 4, 4, ROW, 4, 4, COL, false>(&mulCycles[3]),
+		MeasureMatrixMultiplication<float, 4, 4, COL, 4, 4, ROW, false>(&mulCycles[4]),
+		MeasureMatrixMultiplication<float, 4, 4, COL, 4, 4, COL, false>(&mulCycles[5]),
+		MeasureMatrixMultiplication<double, 4, 4, ROW, 4, 4, ROW, false>(&mulCycles[6]),
+		MeasureMatrixMultiplication<float, 4, 3, ROW, 3, 4, ROW, false>(&mulCycles[7]),
+		MeasureMatrixMultiplication<float, 3, 4, ROW, 4, 3, ROW, false>(&mulCycles[8]),
+		MeasureMatrixMultiplication<float, 3, 4, ROW, 4, 3, ROW, true>(&mulCycles[9]),
 	};
 
 	cout << "[Matrix multiplication]" << endl;
@@ -142,7 +171,7 @@ void Measure() {
 		cout << label << "\t";
 	}
 	cout << endl;
-	for (int i = 0; i < 8 * 7; ++i) {
+	for (int i = 0; i < 8 * 9; ++i) {
 		cout << "-";
 	}
 	cout << endl;
@@ -151,7 +180,8 @@ void Measure() {
 		for (auto config : mulConfigs[i]) {
 			cout << config << "\t";
 		}
-		printf("%.02f ns", mulTimes[i] * 1e9);
+		printf("%-6.02f ns\t", mulTimes[i] * 1e9);
+		printf("%.02f\t", mulCycles[i]);
 		cout << endl;
 	}
 	cout << endl;
@@ -159,8 +189,8 @@ void Measure() {
 
 
 	// Addition measures
-	std::string addLabels[5] = {
-		"type", "dim", "layout", "packed", "time/op"
+	std::string addLabels[6] = {
+		"type", "dim", "layout", "packed", "time/op", "\tcycles"
 	};
 
 	constexpr int addNumCases = 8;
@@ -174,15 +204,16 @@ void Measure() {
 		{ "float",	"3x4",	"R",	"F" },
 		{ "float",	"3x4",	"R",	"T" },
 	};
-	double addTimes[addNumCases] = {
-		MeasureMatrixAddition<float, 1, 1, ROW, false>(),
-		MeasureMatrixAddition<float, 2, 2, ROW, false>(),
-		MeasureMatrixAddition<float, 3, 3, ROW, false>(),
-		MeasureMatrixAddition<float, 4, 4, ROW, false>(),
-		MeasureMatrixAddition<double, 4, 4, ROW, false>(),
-		MeasureMatrixAddition<float, 4, 3, ROW, false>(),
-		MeasureMatrixAddition<float, 3, 4, ROW, false>(),
-		MeasureMatrixAddition<float, 3, 4, ROW, true>(),
+	std::array<double, addNumCases> addCycles;
+	std::array<double, addNumCases> addTimes = {
+		MeasureMatrixAddition<float, 1, 1, ROW, false>(&addCycles[0]),
+		MeasureMatrixAddition<float, 2, 2, ROW, false>(&addCycles[1]),
+		MeasureMatrixAddition<float, 3, 3, ROW, false>(&addCycles[2]),
+		MeasureMatrixAddition<float, 4, 4, ROW, false>(&addCycles[3]),
+		MeasureMatrixAddition<double, 4, 4, ROW, false>(&addCycles[4]),
+		MeasureMatrixAddition<float, 4, 3, ROW, false>(&addCycles[5]),
+		MeasureMatrixAddition<float, 3, 4, ROW, false>(&addCycles[6]),
+		MeasureMatrixAddition<float, 3, 4, ROW, true>(&addCycles[7]),
 	};
 
 	cout << "[Matrix addition]" << endl;
@@ -190,7 +221,7 @@ void Measure() {
 		cout << label << "\t";
 	}
 	cout << endl;
-	for (int i = 0; i < 8 * 7; ++i) {
+	for (int i = 0; i < 8 * 9; ++i) {
 		cout << "-";
 	}
 	cout << endl;
@@ -199,8 +230,15 @@ void Measure() {
 		for (auto config : addConfigs[i]) {
 			cout << config << "\t";
 		}
-		printf("%.02f ns", addTimes[i] * 1e9);
+		printf("%-6.02f ns\t", addTimes[i] * 1e9);
+		printf("%.02f\t", addCycles[i]);
 		cout << endl;
 	}
 	cout << endl;
+
+	// Estimate CPU frequency
+	double sumTime = std::accumulate(mulTimes.begin(), mulTimes.end(), 0.0);
+	double sumCycles = std::accumulate(mulCycles.begin(), mulCycles.end(), 0.0);
+	cout << "Estimated CPU clock frequency: " << (sumCycles / sumTime) / 1e9 << " GHz" << endl;
+
 }
