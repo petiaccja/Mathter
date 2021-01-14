04653b39611cfae935aecd8e036106a6ce5bef0a
Péter Kardos <kardospeter1994@hotmail.com>
2021-01-14 17:50:04 +0100

    simplified conan file

--- conanfile.py
+++ conanfile.py
@@ -2,31 +2,14 @@ from conans import ConanFile, CMake, tools
 
 
 class MathterConan(ConanFile):
-    name = "Mathter"
+    name = "mathter"
     version = "1.0.0"
     license = "The Unlicence"
     author = "Péter Kardos mathter_library@outlook.com"
     url = "https://github.com/petiaccja/Mathter"
     description = "Powerful 3D math and small-matrix linear algebra library for games and science."
     topics = ("game-dev", "linear-algebra", "vector-math", "matrix-library")
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-    generators = "cmake"
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def source(self):
-        self.run("git clone https://github.com/petiaccja/Mathter.git")
-
-    def build(self):
-        pass
+    exports_sources = "Mathter/*"
 
     def package(self):
-        self.copy("*.hpp", dst="include", src="Mathter")
-
-    def package_info(self):
-        self.cpp_info.libs = []
-
+        self.copy("*.hpp", dst="include/Mathter", src="Mathter")
