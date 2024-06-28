42768c5f3b62d0b0faa571496661a7525dab9ea9
Péter Kardos <kardospeter1994@hotmail.com>
2024-06-28 22:48:54 +0200

    more detailed & tidy vector benchmarks

--- benchmark/BenchmarkVectorArithmetic.cpp
+++ benchmark/BenchmarkVectorArithmetic.cpp
@@ -7,128 +7,55 @@
 using namespace mathter;
 
 
-//------------------------------------------------------------------------------
-// Single precision
-//------------------------------------------------------------------------------
-
-// Addition
-BENCHMARK_CASE("Vec2 ADD float vec", "[Vector]", 50, 64, opAdd, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec2f, 32>(), MakeVectorArray<Vec2f, 32>())));
-
-BENCHMARK_CASE("Vec3 ADD float vec", "[Vector]", 50, 64, opAdd, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec3f, 32>(), MakeVectorArray<Vec3f, 32>())));
-
-BENCHMARK_CASE("Vec4 ADD float vec", "[Vector]", 50, 64, opAdd, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec4f, 32>(), MakeVectorArray<Vec4f, 32>())));
-
-BENCHMARK_CASE("Vec2 ADD float packed", "[Vector]", 50, 64, opAdd, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec2fp, 32>(), MakeVectorArray<Vec2fp, 32>())));
-
-BENCHMARK_CASE("Vec3 ADD float packed", "[Vector]", 50, 64, opAdd, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec3fp, 32>(), MakeVectorArray<Vec3fp, 32>())));
-
-BENCHMARK_CASE("Vec4 ADD float packed", "[Vector]", 50, 64, opAdd, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec4fp, 32>(), MakeVectorArray<Vec4fp, 32>())));
+#define VECTOR_BINOP_BENCHMARK_CASE(TYPE, DIM, PACKED, OP, OPTEXT)                  \
+	BENCHMARK_CASE(#TYPE "." #DIM " " OPTEXT " " #TYPE "." #DIM " (P=" #PACKED ")", \
+				   "[Vector]",                                                      \
+				   50,                                                              \
+				   64,                                                              \
+				   OP,                                                              \
+				   feedBinary,                                                      \
+				   (TuplizeArrays(MakeVectorArray<Vector<TYPE, DIM, PACKED>, 32>(), MakeVectorArray<Vector<TYPE, DIM, PACKED>, 32>())));
 
 
-// Multiplication
-BENCHMARK_CASE("Vec2 MUL float vec", "[Vector]", 50, 64, opMul, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec2f, 32>(), MakeVectorArray<Vec2f, 32>())));
 
-BENCHMARK_CASE("Vec3 MUL float vec", "[Vector]", 50, 64, opMul, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec3f, 32>(), MakeVectorArray<Vec3f, 32>())));
+VECTOR_BINOP_BENCHMARK_CASE(float, 2, false, opAdd, "+");
+VECTOR_BINOP_BENCHMARK_CASE(float, 3, false, opAdd, "+");
+VECTOR_BINOP_BENCHMARK_CASE(float, 4, false, opAdd, "+");
+VECTOR_BINOP_BENCHMARK_CASE(float, 2, true, opAdd, "+");
+VECTOR_BINOP_BENCHMARK_CASE(float, 3, true, opAdd, "+");
+VECTOR_BINOP_BENCHMARK_CASE(float, 4, true, opAdd, "+");
 
-BENCHMARK_CASE("Vec4 MUL float vec", "[Vector]", 50, 64, opMul, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec4f, 32>(), MakeVectorArray<Vec4f, 32>())));
+VECTOR_BINOP_BENCHMARK_CASE(float, 2, false, opMul, "*");
+VECTOR_BINOP_BENCHMARK_CASE(float, 3, false, opMul, "*");
+VECTOR_BINOP_BENCHMARK_CASE(float, 4, false, opMul, "*");
+VECTOR_BINOP_BENCHMARK_CASE(float, 2, true, opMul, "*");
+VECTOR_BINOP_BENCHMARK_CASE(float, 3, true, opMul, "*");
+VECTOR_BINOP_BENCHMARK_CASE(float, 4, true, opMul, "*");
 
-BENCHMARK_CASE("Vec2 MUL float packed", "[Vector]", 50, 64, opMul, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec2fp, 32>(), MakeVectorArray<Vec2fp, 32>())));
+VECTOR_BINOP_BENCHMARK_CASE(float, 2, false, opDiv, "/");
+VECTOR_BINOP_BENCHMARK_CASE(float, 3, false, opDiv, "/");
+VECTOR_BINOP_BENCHMARK_CASE(float, 4, false, opDiv, "/");
+VECTOR_BINOP_BENCHMARK_CASE(float, 2, true, opDiv, "/");
+VECTOR_BINOP_BENCHMARK_CASE(float, 3, true, opDiv, "/");
+VECTOR_BINOP_BENCHMARK_CASE(float, 4, true, opDiv, "/");
 
-BENCHMARK_CASE("Vec3 MUL float packed", "[Vector]", 50, 64, opMul, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec3fp, 32>(), MakeVectorArray<Vec3fp, 32>())));
+VECTOR_BINOP_BENCHMARK_CASE(double, 2, false, opAdd, "+");
+VECTOR_BINOP_BENCHMARK_CASE(double, 3, false, opAdd, "+");
+VECTOR_BINOP_BENCHMARK_CASE(double, 4, false, opAdd, "+");
+VECTOR_BINOP_BENCHMARK_CASE(double, 2, true, opAdd, "+");
+VECTOR_BINOP_BENCHMARK_CASE(double, 3, true, opAdd, "+");
+VECTOR_BINOP_BENCHMARK_CASE(double, 4, true, opAdd, "+");
 
-BENCHMARK_CASE("Vec4 MUL float packed", "[Vector]", 50, 64, opMul, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec4fp, 32>(), MakeVectorArray<Vec4fp, 32>())));
+VECTOR_BINOP_BENCHMARK_CASE(double, 2, false, opMul, "*");
+VECTOR_BINOP_BENCHMARK_CASE(double, 3, false, opMul, "*");
+VECTOR_BINOP_BENCHMARK_CASE(double, 4, false, opMul, "*");
+VECTOR_BINOP_BENCHMARK_CASE(double, 2, true, opMul, "*");
+VECTOR_BINOP_BENCHMARK_CASE(double, 3, true, opMul, "*");
+VECTOR_BINOP_BENCHMARK_CASE(double, 4, true, opMul, "*");
 
-
-// Division
-BENCHMARK_CASE("Vec2 DIV float vec", "[Vector]", 50, 64, opDiv, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec2f, 32>(), MakeVectorArray<Vec2f, 32>())));
-
-BENCHMARK_CASE("Vec3 DIV float vec", "[Vector]", 50, 64, opDiv, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec3f, 32>(), MakeVectorArray<Vec3f, 32>())));
-
-BENCHMARK_CASE("Vec4 DIV float vec", "[Vector]", 50, 64, opDiv, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec4f, 32>(), MakeVectorArray<Vec4f, 32>())));
-
-BENCHMARK_CASE("Vec2 DIV float packed", "[Vector]", 50, 64, opDiv, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec2fp, 32>(), MakeVectorArray<Vec2fp, 32>())));
-
-BENCHMARK_CASE("Vec3 DIV float packed", "[Vector]", 50, 64, opDiv, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec3fp, 32>(), MakeVectorArray<Vec3fp, 32>())));
-
-BENCHMARK_CASE("Vec4 DIV float packed", "[Vector]", 50, 64, opDiv, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec4fp, 32>(), MakeVectorArray<Vec4fp, 32>())));
-
-//------------------------------------------------------------------------------
-// Double precision
-//------------------------------------------------------------------------------
-
-// Addition
-BENCHMARK_CASE("Vec2 ADD double vec", "[Vector]", 50, 64, opAdd, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec2d, 32>(), MakeVectorArray<Vec2d, 32>())));
-
-BENCHMARK_CASE("Vec3 ADD double vec", "[Vector]", 50, 64, opAdd, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec3d, 32>(), MakeVectorArray<Vec3d, 32>())));
-
-BENCHMARK_CASE("Vec4 ADD double vec", "[Vector]", 50, 64, opAdd, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec4d, 32>(), MakeVectorArray<Vec4d, 32>())));
-
-BENCHMARK_CASE("Vec2 ADD double packed", "[Vector]", 50, 64, opAdd, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec2dp, 32>(), MakeVectorArray<Vec2dp, 32>())));
-
-BENCHMARK_CASE("Vec3 ADD double packed", "[Vector]", 50, 64, opAdd, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec3dp, 32>(), MakeVectorArray<Vec3dp, 32>())));
-
-BENCHMARK_CASE("Vec4 ADD double packed", "[Vector]", 50, 64, opAdd, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec4dp, 32>(), MakeVectorArray<Vec4dp, 32>())));
-
-
-// Multiplication
-BENCHMARK_CASE("Vec2 MUL double vec", "[Vector]", 50, 64, opMul, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec2d, 32>(), MakeVectorArray<Vec2d, 32>())));
-
-BENCHMARK_CASE("Vec3 MUL double vec", "[Vector]", 50, 64, opMul, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec3d, 32>(), MakeVectorArray<Vec3d, 32>())));
-
-BENCHMARK_CASE("Vec4 MUL double vec", "[Vector]", 50, 64, opMul, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec4d, 32>(), MakeVectorArray<Vec4d, 32>())));
-
-BENCHMARK_CASE("Vec2 MUL double packed", "[Vector]", 50, 64, opMul, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec2dp, 32>(), MakeVectorArray<Vec2dp, 32>())));
-
-BENCHMARK_CASE("Vec3 MUL double packed", "[Vector]", 50, 64, opMul, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec3dp, 32>(), MakeVectorArray<Vec3dp, 32>())));
-
-BENCHMARK_CASE("Vec4 MUL double packed", "[Vector]", 50, 64, opMul, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec4dp, 32>(), MakeVectorArray<Vec4dp, 32>())));
-
-
-// Division
-BENCHMARK_CASE("Vec2 DIV double vec", "[Vector]", 50, 64, opDiv, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec2d, 32>(), MakeVectorArray<Vec2d, 32>())));
-
-BENCHMARK_CASE("Vec3 DIV double vec", "[Vector]", 50, 64, opDiv, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec3d, 32>(), MakeVectorArray<Vec3d, 32>())));
-
-BENCHMARK_CASE("Vec4 DIV double vec", "[Vector]", 50, 64, opDiv, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec4d, 32>(), MakeVectorArray<Vec4d, 32>())));
-
-BENCHMARK_CASE("Vec2 DIV double packed", "[Vector]", 50, 64, opDiv, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec2dp, 32>(), MakeVectorArray<Vec2dp, 32>())));
-
-BENCHMARK_CASE("Vec3 DIV double packed", "[Vector]", 50, 64, opDiv, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec3dp, 32>(), MakeVectorArray<Vec3dp, 32>())));
-
-BENCHMARK_CASE("Vec4 DIV double packed", "[Vector]", 50, 64, opDiv, feedBinary,
-			   (TuplizeArrays(MakeVectorArray<Vec4dp, 32>(), MakeVectorArray<Vec4dp, 32>())));
+VECTOR_BINOP_BENCHMARK_CASE(double, 2, false, opDiv, "/");
+VECTOR_BINOP_BENCHMARK_CASE(double, 3, false, opDiv, "/");
+VECTOR_BINOP_BENCHMARK_CASE(double, 4, false, opDiv, "/");
+VECTOR_BINOP_BENCHMARK_CASE(double, 2, true, opDiv, "/");
+VECTOR_BINOP_BENCHMARK_CASE(double, 3, true, opDiv, "/");
+VECTOR_BINOP_BENCHMARK_CASE(double, 4, true, opDiv, "/");
