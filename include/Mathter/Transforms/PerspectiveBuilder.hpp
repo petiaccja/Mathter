c3d01852e817c84100026da2e1dcb41f5457aeb1
Péter Kardos <kardospeter1994@hotmail.com>
2024-08-01 23:37:51 +0200

    test perspective builder

--- include/Mathter/Transforms/PerspectiveBuilder.hpp
+++ include/Mathter/Transforms/PerspectiveBuilder.hpp
@@ -1,75 +1,79 @@
-﻿// L=============================================================================
+// L=============================================================================
 // L This software is distributed under the MIT license.
 // L Copyright 2021 Péter Kardos
 // L=============================================================================
 
 #pragma once
 
-#include "../Matrix/MatrixImpl.hpp"
+#include "../Matrix/Matrix.hpp"
 #include "../Vector.hpp"
-#include "IdentityBuilder.hpp"
+#include "ZeroBuilder.hpp"
 
 namespace mathter {
 
+namespace impl {
 
-template <class T, int Dim, bool Packed>
-class PerspectiveBuilder {
-	static_assert(!std::is_integral_v<T>);
+	template <class T, int Dim, bool Packed>
+	class PerspectiveBuilder {
+		static_assert(!std::is_integral_v<T>, "Perspective transformations require real numbers.");
+		static_assert(!is_complex_v<T>, "Perspective transformations cannot be done in the complex domain.");
 
-public:
-	PerspectiveBuilder(T fovX, const Vector<T, Dim - 1, Packed>& ratios, T nearPlane, T farPlane, T projNearPlane = 0, T projFarPlane = 1)
-		: fovX(fovX), ratios(ratios), nearPlane(nearPlane), farPlane(farPlane), projNearPlane(projNearPlane), projFarPlane(projFarPlane) {}
-	PerspectiveBuilder& operator=(const PerspectiveBuilder&) = delete;
+	public:
+		PerspectiveBuilder(T fovX, const Vector<T, Dim - 1, Packed>& ratios, T nearPlane, T farPlane, T projNearPlane = 0, T projFarPlane = 1)
+			: fovX(fovX), ratios(ratios), nearPlane(nearPlane), farPlane(farPlane), projNearPlane(projNearPlane), projFarPlane(projFarPlane) {}
+		PerspectiveBuilder& operator=(const PerspectiveBuilder&) = delete;
 
-	template <class U, eMatrixOrder Order, eMatrixLayout Layout, bool MPacked>
-	operator Matrix<U, Dim + 1, Dim + 1, Order, Layout, MPacked>() const {
-		Matrix<U, Dim + 1, Dim + 1, Order, Layout, MPacked> m;
-		Set(m);
-		return m;
-	}
-
-private:
-	template <class U, int Rows, int Columns, eMatrixOrder Order, eMatrixLayout Layout, bool MPacked>
-	void Set(Matrix<U, Rows, Columns, Order, Layout, MPacked>& m) const {
-		assert((nearPlane < 0 && farPlane < nearPlane) || (0 < nearPlane && nearPlane < farPlane));
-
-		using UVec = Vector<U, Dim - 1, Packed>;
-
-		m = Zero();
-		// Layout be like (precede_vector):
-		// w 0 0 0
-		// 0 h 0 0
-		// 0 0 A B
-		// 0 0 C 0
-		auto absFovX = std::abs(U(fovX));
-		U N = U(nearPlane);
-		U F = U(farPlane);
-		U n = U(projNearPlane);
-		U f = U(projFarPlane);
-		U C = U(nearPlane) < U(0) ? U(-1) : U(1);
-		U A = C * (f * F - n * N) / (F - N);
-		U B = C * F * N * (n - f) / (F - N);
-		UVec adjRatios = U(ratios(0)) / UVec(ratios);
-		U w = tan(U(0.5) * absFovX);
-		adjRatios /= w;
-		for (int i = 0; i < adjRatios.Dimension(); ++i) {
-			m(i, i) = adjRatios(i);
+		template <class U, eMatrixOrder Order, eMatrixLayout Layout, bool MPacked>
+		operator Matrix<U, Dim + 1, Dim + 1, Order, Layout, MPacked>() const {
+			Matrix<U, Dim + 1, Dim + 1, Order, Layout, MPacked> m;
+			Set(m);
+			return m;
 		}
-		m(m.RowCount() - 2, m.ColumnCount() - 2) = A;
-		if (Order == eMatrixOrder::FOLLOW_VECTOR) {
-			std::swap(B, C);
-		}
-		m(m.RowCount() - 2, m.ColumnCount() - 1) = B;
-		m(m.RowCount() - 1, m.ColumnCount() - 2) = C;
-	}
 
-	const T fovX;
-	const Vector<T, Dim - 1, Packed> ratios;
-	const T nearPlane;
-	const T farPlane;
-	const T projNearPlane = 0;
-	const T projFarPlane = 1;
-};
+	private:
+		template <class U, int Rows, int Columns, eMatrixOrder Order, eMatrixLayout Layout, bool MPacked>
+		void Set(Matrix<U, Rows, Columns, Order, Layout, MPacked>& m) const {
+			assert((nearPlane < 0 && farPlane < nearPlane) || (0 < nearPlane && nearPlane < farPlane));
+
+			using UVec = Vector<U, Dim - 1, Packed>;
+
+			m = Zero();
+			// Layout be like (precede_vector):
+			// w 0 0 0
+			// 0 h 0 0
+			// 0 0 A B
+			// 0 0 C 0
+			auto absFovX = std::abs(U(fovX));
+			const auto N = U(nearPlane);
+			const auto F = U(farPlane);
+			const auto n = U(projNearPlane);
+			const auto f = U(projFarPlane);
+			auto C = U(nearPlane) < U(0) ? U(-1) : U(1);
+			const auto A = C * (f * F - n * N) / (F - N);
+			auto B = C * F * N * (n - f) / (F - N);
+			auto adjRatios = U(ratios(0)) / UVec(ratios);
+			const auto w = std::tan(U(0.5) * absFovX);
+			adjRatios /= w;
+			for (int i = 0; i < adjRatios.Dimension(); ++i) {
+				m(i, i) = adjRatios(i);
+			}
+			m(m.RowCount() - 2, m.ColumnCount() - 2) = A;
+			if (Order == eMatrixOrder::FOLLOW_VECTOR) {
+				std::swap(B, C);
+			}
+			m(m.RowCount() - 2, m.ColumnCount() - 1) = B;
+			m(m.RowCount() - 1, m.ColumnCount() - 2) = C;
+		}
+
+		const T fovX;
+		const Vector<T, Dim - 1, Packed> ratios;
+		const T nearPlane;
+		const T farPlane;
+		const T projNearPlane = 0;
+		const T projFarPlane = 1;
+	};
+
+} // namespace impl
 
 
 /// <summary> Creates a general, n-dimensional perspective projection matrix. </summary>
@@ -86,7 +90,7 @@ private:
 template <class T, int DimMinus1, bool Packed>
 auto Perspective(T fovX, const Vector<T, DimMinus1, Packed>& ratios, T nearPlane, T farPlane, T projNearPlane, T projFarPlane) {
 	using NonIntegral = std::conditional_t<std::is_integral_v<T>, float, T>;
-	return PerspectiveBuilder<NonIntegral, DimMinus1 + 1, Packed>{ fovX, ratios, nearPlane, farPlane, projNearPlane, projFarPlane };
+	return impl::PerspectiveBuilder<NonIntegral, DimMinus1 + 1, Packed>{ fovX, ratios, nearPlane, farPlane, projNearPlane, projFarPlane };
 }
 
 
@@ -106,10 +110,10 @@ auto Perspective(T fov, T nearPlane, T farPlane, T projNearPlane, T projFarPlane
 
 
 /// <summary> Creates a 3D projection matrix. </summary>
-/// <param name="fov"/> Field of view. </param>
+/// <param name="fovX"/> Field of view. </param>
 /// <param name="aspectRatio"> FovX/FovY, so 1.777 for a 16:9 screen. </param>
-/// <param name="nearPlane"/> Lower bound of the volume on the Y axis. </param>
-/// <param name="farPlane"/> Upper bound of the volume on the Y axis. </param>
+/// <param name="nearPlane"/> Lower bound of the volume on the Z axis. </param>
+/// <param name="farPlane"/> Upper bound of the volume on the Z axis. </param>
 /// <param name="projNearPlane"/> Near plane is taken here after projection. </param>
 /// <param name="projFarPlane"/> Far plane is taken here after projection. </param>
 /// <remarks> The pre-projection near and far planes must be on the same side of the last axis, i.e. both negative or both positive.
@@ -117,10 +121,11 @@ auto Perspective(T fov, T nearPlane, T farPlane, T projNearPlane, T projFarPlane
 ///		one positive the other negative, etc.
 ///		The <paramref name="aspectRatio"/> can be negative, in which case the second axis (i.e. Y) is inverted. </remarks>
 template <class T>
-auto Perspective(T fov, T aspectRatio, T nearPlane, T farPlane, T projNearPlane, T projFarPlane) {
-	return Perspective(std::abs(fov), Vector<T, 2, false>{ fov < T(0) ? T(-1) : T(1), T(1) / aspectRatio }, nearPlane, farPlane, projNearPlane, projFarPlane);
+auto Perspective(T fovX, T aspectRatio, T nearPlane, T farPlane, T projNearPlane, T projFarPlane) {
+	const auto ratioX = fovX < T(0) ? T(-1) : T(1);
+	const auto ratioY = T(1) / aspectRatio;
+	const auto absFovX = std::abs(fovX);
+	return Perspective(absFovX, Vector<T, 2, false>{ ratioX, ratioY }, nearPlane, farPlane, projNearPlane, projFarPlane);
 }
-
-
 
 } // namespace mathter
---
+++ test/Transforms/TestPerspectiveBuilder.cpp
@@ -0,0 +1,137 @@
+// L=============================================================================
+// L This software is distributed under the MIT license.
+// L Copyright 2024 Péter Kardos
+// L=============================================================================
+
+#pragma warning(disable : 4244)
+
+#include "../ApplyTransform.hpp"
+#include "../Approx.hpp"
+#include "../Cases.hpp"
+
+#include <Mathter/Transforms/PerspectiveBuilder.hpp>
+
+#include <catch2/catch_template_test_macros.hpp>
+
+
+using namespace mathter;
+using namespace test_util;
+using namespace std::complex_literals;
+
+
+template <class Mat, class Builder, class Vec>
+void TestCaseOrthographicMatrix(const Builder& builder, const Vec& testPoint, const Vec& expectedPoint) {
+	const Mat m = builder;
+	const auto result = ApplyTransform(m, testPoint);
+	REQUIRE(result == test_util::Approx(expectedPoint, 2e-6f));
+}
+
+
+TEMPLATE_LIST_TEST_CASE("Transform: Perspective 3D / generic", "[Transforms]",
+						decltype(MatrixCaseList<ScalarsFloating, OrdersAll, LayoutsAll, PackingsAll>{})) {
+	using ExampleMat = typename TestType::template Matrix<1, 1>;
+	using Mat = typename TestType::template Matrix<4, 4>;
+	using Scalar = scalar_type_t<ExampleMat>;
+	using Vec = Vector<Scalar, 3, is_packed_v<ExampleMat>>;
+
+	const Scalar fovX(0.92729521800161); // A 1 unit wide piece at 2 units away fits the screen exactly.
+	const Scalar aspectRatio(2.0);
+
+	const std::array projPlanes = {
+		std::pair{ Scalar(1), Scalar(2) },
+		std::pair{ Scalar(2), Scalar(1) },
+		std::pair{ Scalar(-1), Scalar(-2) },
+		std::pair{ Scalar(-2), Scalar(-1) },
+		std::pair{ Scalar(-1), Scalar(1) },
+		std::pair{ Scalar(1), Scalar(-1) },
+	};
+
+	SECTION("Positive near & far plane") {
+		const Scalar nearPlane(0.5);
+		const Scalar farPlane(2.0);
+
+		for (const auto& [projNearPlane, projFarPlane] : projPlanes) {
+			const std::array<Vec, 3> testPoints = {
+				Vec(-0.25f, -0.125f, nearPlane),
+				Vec(1.0f, 0.5f, farPlane),
+				Vec(0.4f, 0.1f, 1.6f),
+			};
+
+			const std::array<Vec, 2> expectedPoints = {
+				Vec(-1.0f, -1.0f, projNearPlane),
+				Vec(1.0f, 1.0f, projFarPlane),
+			};
+
+			const auto builder = Perspective(fovX, aspectRatio, nearPlane, farPlane, projNearPlane, projFarPlane);
+			const Mat m = builder;
+			REQUIRE(ApplyTransform(m, testPoints[0]) == test_util::Approx(expectedPoints[0], 1e-6f));
+			REQUIRE(ApplyTransform(m, testPoints[1]) == test_util::Approx(expectedPoints[1], 1e-6f));
+			const auto insidePoint = ApplyTransform(m, testPoints[2]);
+			REQUIRE(std::abs(insidePoint[0]) <= Scalar(1.0));
+			REQUIRE(std::abs(insidePoint[1]) <= Scalar(1.0));
+			REQUIRE(std::min(projNearPlane, projFarPlane) <= insidePoint[2]);
+			REQUIRE(insidePoint[2] <= std::max(projNearPlane, projFarPlane));
+		}
+	}
+	SECTION("Negative near & far plane") {
+		const Scalar nearPlane(-0.5);
+		const Scalar farPlane(-2.0);
+
+		for (const auto& [projNearPlane, projFarPlane] : projPlanes) {
+			const std::array testPoints = {
+				Vec(-0.25f, -0.125f, nearPlane),
+				Vec(1.0f, 0.5f, farPlane),
+				Vec(0.4f, 0.1f, -1.6f),
+			};
+
+			const std::array expectedPoints = {
+				Vec(-1.0f, -1.0f, projNearPlane),
+				Vec(1.0f, 1.0f, projFarPlane),
+			};
+
+			const auto builder = Perspective(fovX, aspectRatio, nearPlane, farPlane, projNearPlane, projFarPlane);
+			const Mat m = builder;
+			REQUIRE(ApplyTransform(m, testPoints[0]) == test_util::Approx(expectedPoints[0], 1e-6f));
+			REQUIRE(ApplyTransform(m, testPoints[1]) == test_util::Approx(expectedPoints[1], 1e-6f));
+			const auto insidePoint = ApplyTransform(m, testPoints[2]);
+			REQUIRE(std::abs(insidePoint[0]) <= Scalar(1.0));
+			REQUIRE(std::abs(insidePoint[1]) <= Scalar(1.0));
+			REQUIRE(std::min(projNearPlane, projFarPlane) <= insidePoint[2]);
+			REQUIRE(insidePoint[2] <= std::max(projNearPlane, projFarPlane));
+		}
+	}
+}
+
+
+TEMPLATE_LIST_TEST_CASE("Transform: Perspective 2D", "[Transforms]",
+						decltype(MatrixCaseList<ScalarsFloating, OrdersAll, LayoutsAll, PackingsAll>{})) {
+	// This test does not need to test all combinations exhaustively, the generic function
+	// is tested exhaustively via the 3D test.
+	// This is just to ensure the 2D interface works as intended.
+
+	using ExampleMat = typename TestType::template Matrix<1, 1>;
+	using Mat = typename TestType::template Matrix<3, 3>;
+	using Scalar = scalar_type_t<ExampleMat>;
+	using Vec = Vector<Scalar, 2, is_packed_v<ExampleMat>>;
+
+	const Scalar fovX(0.92729521800161); // A 1 unit wide piece at 2 units away fits the screen exactly.
+	const Scalar nearPlane(0.5);
+	const Scalar farPlane(2);
+	const Scalar projNearPlane(0);
+	const Scalar projFarPlane(1);
+	const auto builder = Perspective(fovX, nearPlane, farPlane, projNearPlane, projFarPlane);
+
+	const std::array testPoints = {
+		Vec(-0.25f, nearPlane),
+		Vec(1.0f, farPlane),
+	};
+
+	const std::array expectedPoints = {
+		Vec(-1.0f, projNearPlane),
+		Vec(1.0f, projFarPlane),
+	};
+
+	const Mat m = builder;
+	REQUIRE(ApplyTransform(m, testPoints[0]) == test_util::Approx(expectedPoints[0], 1e-6f));
+	REQUIRE(ApplyTransform(m, testPoints[1]) == test_util::Approx(expectedPoints[1], 1e-6f));
+}
