a0787787288d4b559af4f3b46b420649f2d543e5
Péter Kardos <kardospeter1994@hotmail.com>
2024-08-05 12:27:34 +0200

    stub

---
+++ include/Mathter/Geometry/Intersection.hpp
@@ -0,0 +1,23 @@
+// L=============================================================================
+// L This software is distributed under the MIT license.
+// L Copyright 2024 Péter Kardos
+// L=============================================================================
+
+#pragma once
+
+#include "Line.hpp"
+
+#include <optional>
+
+
+namespace mathter {
+
+
+/// <summary> Intersect lines in 2 dimensions. </summary>
+template <class T1, class T2>
+void Intersect(const Line<T1, 2>& lhs, const Line<T2, 2>& rhs) {
+	
+}
+
+
+}
