0cf707c9c8b3ef8a2dcdd39898c816b0acd4f2c7
Péter Kardos <kardospeter1994@hotmail.com>
2024-09-09 16:25:00 +0200

    rewrite iostream and add tests

--- include/Mathter/IoStream.hpp
+++ include/Mathter/IoStream.hpp
@@ -5,233 +5,237 @@
 
 #pragma once
 
+#include "Common/TypeTraits.hpp"
+#include "Matrix/Matrix.hpp"
+#include "Quaternion/Math.hpp"
+#include "Quaternion/Quaternion.hpp"
+#include "Utility.hpp"
 #include "Vector/Vector.hpp"
 
 #include <iostream>
+#include <type_traits>
 
 namespace mathter {
 
-enum class eEnclosingBracket {
-	NONE,
-	PARANTHESE,
-	BRACKET,
-	BRACE,
-};
-
-/// <summary> Prints the vector like [1,2,3]. </summary>
-template <class T, int Dim, bool Packed>
-std::ostream& operator<<(std::ostream& os, const mathter::Vector<T, Dim, Packed>& v) {
-	os << "[";
-	for (int x = 0; x < Dim; ++x) {
-		os << v(x) << (x == Dim - 1 ? "" : ", ");
-	}
-	os << "]";
-	return os;
-}
-
 
 namespace impl {
-	template <class T>
-	struct dependent_false {
-		static constexpr bool value = false;
-	};
-	template <class T>
-	constexpr bool dependent_false_v = dependent_false<T>::value;
 
-	template <class AritT, typename std::enable_if<std::is_integral<AritT>::value && std::is_signed<AritT>::value, int>::type = 0>
-	AritT strtonum(const char* str, const char** end) {
-		AritT value;
-		value = (AritT)strtoll(str, (char**)end, 10);
-		return value;
-	}
-	template <class AritT, typename std::enable_if<std::is_integral<AritT>::value && !std::is_signed<AritT>::value, int>::type = 0>
-	AritT strtonum(const char* str, const char** end) {
-		AritT value;
-		value = (AritT)strtoull(str, (char**)end, 10);
-		return value;
-	}
-	template <class AritT, typename std::enable_if<std::is_floating_point<AritT>::value, int>::type = 0>
-	AritT strtonum(const char* str, const char** end) {
-		AritT value;
-		value = (AritT)strtold(str, (char**)end);
-		return value;
+	template <class Char, class CharTraits>
+	char ReadWhileSpace(std::basic_istream<Char, CharTraits>& is) {
+		char c = Char('\0');
+		do {
+			if (!is.good()) {
+				throw std::runtime_error("input stream failed");
+			}
+			c = is.peek();
+			if (std::isspace(c)) {
+				is.get();
+			}
+		} while (std::isspace(c));
+		return c;
 	}
 
-	inline const char* StripSpaces(const char* str) {
-		while (*str != '\0' && isspace(*str))
-			++str;
+
+	template <class Char, class CharTraits>
+	std::basic_string<Char, CharTraits> ReadArray(std::basic_istream<Char, CharTraits>& is) {
+		std::basic_string<Char, CharTraits> str;
+		size_t depth = 0;
+		do {
+			const auto c = static_cast<char>(is.get());
+			if (!is.good()) {
+				throw std::invalid_argument("unexpected end of stream");
+			}
+			if (c == Char('[')) {
+				depth += 1;
+			}
+			else if (c == Char(']')) {
+				depth -= 1;
+			}
+			str += c;
+		} while (depth > 0);
 		return str;
-	};
+	}
+
+
+	template <class Char, class CharTraits>
+	std::vector<std::string_view> SplitArray(std::basic_string_view<Char, CharTraits> str, Char sep = ',') {
+		std::vector<std::basic_string_view<Char, CharTraits>> substrings;
+		auto prev = 0;
+		size_t depth = 0;
+		for (size_t i = 0; i < str.size(); ++i) {
+			if (str[i] == '[') {
+				depth += 1;
+			}
+			else if (str[i] == ']') {
+				depth -= 1;
+			}
+			else if (depth == 0 && str[i] == sep) {
+				substrings.push_back(str.substr(prev, i - prev));
+				prev = i + 1;
+			}
+		}
+		substrings.push_back(str.substr(prev, str.npos));
+		return substrings;
+	}
+
+
+	template <class Char, class CharTraits,
+			  class Element>
+	void PrintElement(std::basic_ostream<Char, CharTraits>& os, const Element& value) {
+		if constexpr (std::is_integral_v<Element>) {
+			os << value;
+		}
+		else if constexpr (std::is_floating_point_v<Element>) {
+			os << value;
+		}
+		else if constexpr (is_complex_v<Element>) {
+			os << std::real(value);
+			if (std::imag(value) != remove_complex_t<Element>(0)) {
+				os << " + " << std::imag(value) << "j";
+			}
+		}
+		else {
+			os << value;
+		}
+	}
+
+
+	template <class Char, class CharTraits,
+			  class Element>
+	void ParseElement(std::basic_istream<Char, CharTraits>& is, Element& value) {
+		if constexpr (std::is_integral_v<Element>) {
+			is >> value;
+		}
+		else if constexpr (std::is_floating_point_v<Element>) {
+			is >> value;
+		}
+		else if constexpr (is_complex_v<Element>) {
+			remove_complex_t<Element> re(0);
+			remove_complex_t<Element> im(0);
+			is >> re;
+			const char next = ReadWhileSpace(is);
+			if (next == '+') {
+				is.get();
+				is >> im;
+			}
+			value = { re, im };
+		}
+		else {
+			is >> value;
+		}
+	}
+
+
+	template <class Char, class CharTraits,
+			  class Element, size_t Size>
+	void PrintArray(std::basic_ostream<Char, CharTraits>& os, const std::array<Element, Size>& array) {
+		os << "[";
+		for (const auto& element : array) {
+			PrintElement(os, element);
+			&element != &array.back() ? static_cast<void>(os << ", ") : void();
+		}
+		os << "]";
+	}
+
+
+	template <class Char, class CharTraits,
+			  class Element, size_t Size>
+	void ParseArray(std::basic_istream<Char, CharTraits>& is, std::array<Element, Size>& array) {
+		const char next = ReadWhileSpace(is);
+		if (next != '[') {
+			throw std::invalid_argument("expectd '['");
+		}
+
+		const auto str = ReadArray(is);
+
+		const auto elements = SplitArray(std::basic_string_view<Char, CharTraits>(str).substr(1, str.size() - 2));
+		if (elements.size() != array.size()) {
+			throw std::invalid_argument("incorrect number of array elements");
+		}
+		for (size_t i = 0; i < array.size(); ++i) {
+			std::basic_istringstream<Char, CharTraits> iss;
+			iss.str(std::basic_string<Char, CharTraits>(elements[i]));
+			ParseElement(iss, array[i]);
+		}
+	}
 
 } // namespace impl
 
-/// <summary> Parses a vector from a string. </summary>
-template <class Vec>
-Vec strtovec(const char* str, const char** end) {
-	Vec ret;
 
-	const char* strproc = str;
-
-	// parse initial bracket if any
-	strproc = impl::StripSpaces(strproc);
-	if (*strproc == '\0') {
-		*end = str;
-		return ret;
-	}
-
-	char startBracket = *strproc;
-	char endBracket;
-	bool hasBrackets = false;
-	switch (startBracket) {
-		case '(':
-			endBracket = ')';
-			hasBrackets = true;
-			++strproc;
-			break;
-		case '[':
-			endBracket = ']';
-			hasBrackets = true;
-			++strproc;
-			break;
-		case '{':
-			endBracket = '}';
-			hasBrackets = true;
-			++strproc;
-			break;
-	}
-
-	// parse elements
-	for (int i = 0; i < dimension_v<Vec>; ++i) {
-		const char* elemend;
-		const auto elem = impl::strtonum<scalar_type_t<Vec>>(strproc, &elemend);
-		if (elemend == strproc) {
-			*end = str;
-			return ret;
-		}
-		else {
-			ret[i] = elem;
-			strproc = elemend;
-		}
-		strproc = impl::StripSpaces(strproc);
-		if (*strproc == ',') {
-			++strproc;
-		}
-	}
-
-	// parse ending bracket corresponding to initial bracket
-	if (hasBrackets) {
-		strproc = impl::StripSpaces(strproc);
-		if (*strproc != endBracket) {
-			*end = str;
-			return ret;
-		}
-		++strproc;
-	}
-
-	*end = strproc;
-	return ret;
-}
-
-
-template <class T, int Rows, int Columns, eMatrixOrder Order, eMatrixLayout Layout, bool Packed>
-std::ostream& operator<<(std::ostream& os, const Matrix<T, Rows, Columns, Order, Layout, Packed>& mat) {
-	os << "[";
-	for (int i = 0; i < mat.Height(); ++i) {
-		for (int j = 0; j < mat.Width(); ++j) {
-			os << mat(i, j) << (j == mat.Width() - 1 ? "" : ", ");
-		}
-		if (i < Rows - 1) {
-			os << "; ";
-		}
-	}
-	os << "]";
+template <class Char, class CharTraits,
+		  class T, int Dim, bool Packed>
+auto operator<<(std::basic_ostream<Char, CharTraits>& os, const Vector<T, Dim, Packed>& v) -> std::basic_ostream<Char, CharTraits>& {
+	const auto packed = Vector<T, Dim, true>(v);
+	impl::PrintArray(os, packed.elements.array);
 	return os;
 }
 
 
-template <class Mat>
-Mat strtomat(const char* str, const char** end) {
-	using Vec = Vector<scalar_type_t<Mat>, column_count_v<Mat>, is_packed_v<Mat>>;
-	Mat ret;
-
-	const char* strproc = str;
-
-	// parse initial bracket if any
-	strproc = impl::StripSpaces(strproc);
-	if (*strproc == '\0') {
-		*end = str;
-		return ret;
+template <class Char, class CharTraits,
+		  class T, int Rows, int Columns, eMatrixOrder Order, eMatrixLayout Layout, bool Packed>
+auto operator<<(std::basic_ostream<Char, CharTraits>& os, const Matrix<T, Rows, Columns, Order, Layout, Packed>& m) -> std::basic_ostream<Char, CharTraits>& {
+	std::array<Vector<T, Columns, Packed>, Rows> rows;
+	for (size_t row = 0; row < Rows; ++row) {
+		rows[row] = m.Row(row);
 	}
-
-	char startBracket = *strproc;
-	char endBracket;
-	bool hasBrackets = false;
-	switch (startBracket) {
-		case '(':
-			endBracket = ')';
-			hasBrackets = true;
-			++strproc;
-			break;
-		case '[':
-			endBracket = ']';
-			hasBrackets = true;
-			++strproc;
-			break;
-		case '{':
-			endBracket = '}';
-			hasBrackets = true;
-			++strproc;
-			break;
-	}
-
-	// parse rows
-	for (int i = 0; i < row_count_v<Mat>; ++i) {
-		const char* rowend;
-		const auto row = strtovec<Vec>(strproc, &rowend);
-		if (rowend == strproc) {
-			*end = str;
-			return ret;
-		}
-		else {
-			ret.Row(i) = row;
-			strproc = rowend;
-		}
-		strproc = impl::StripSpaces(strproc);
-		if (i < row_count_v<Mat> - 1) {
-			if (*strproc == ';') {
-				++strproc;
-			}
-			else {
-				*end = str;
-				return ret;
-			}
-		}
-	}
-
-	// parse ending bracket corresponding to initial bracket
-	if (hasBrackets) {
-		strproc = impl::StripSpaces(strproc);
-		if (*strproc != endBracket) {
-			*end = str;
-			return ret;
-		}
-		++strproc;
-	}
-
-	*end = strproc;
-	return ret;
+	impl::PrintArray(os, rows);
+	return os;
 }
 
 
-template <class T, eQuaternionLayout Layout, bool Packed>
-std::ostream& operator<<(std::ostream& os, const Quaternion<T, Layout, Packed>& q) {
-	os << "["
-	   << Angle(q) * T(180.0) / T(3.1415926535897932384626)
-	   << " deg @ "
+template <class Char, class CharTraits,
+		  class T, eQuaternionLayout Layout, bool Packed>
+auto operator<<(std::basic_ostream<Char, CharTraits>& os, const Quaternion<T, Layout, Packed>& q) -> std::basic_ostream<Char, CharTraits>& {
+	os << Char('[')
+	   << Rad2Deg(Angle(q)) << " deg"
+	   << Char(' ') << Char('@') << Char(' ')
 	   << Axis(q)
-	   << "]";
+	   << Char(']');
 	return os;
 }
 
+
+template <class Char, class CharTraits,
+		  class T, int Dim, bool Packed>
+auto operator>>(std::basic_istream<Char, CharTraits>& is, Vector<T, Dim, Packed>& v) -> std::basic_istream<Char, CharTraits>& {
+	auto packed = Vector<T, Dim, true>();
+	impl::ParseArray(is, packed.elements.array);
+	v = Vector<T, Dim, Packed>(packed);
+	return is;
+}
+
+
+template <class Char, class CharTraits,
+		  class T, int Rows, int Columns, eMatrixOrder Order, eMatrixLayout Layout, bool Packed>
+auto operator>>(std::basic_istream<Char, CharTraits>& is, Matrix<T, Rows, Columns, Order, Layout, Packed>& m) -> std::basic_istream<Char, CharTraits>& {
+	std::array<Vector<T, Columns, Packed>, Rows> rows;
+	impl::ParseArray(is, rows);
+	for (size_t row = 0; row < Rows; ++row) {
+		m.Row(row, rows[row]);
+	}
+	return is;
+}
+
+
+template <class Char, class CharTraits,
+		  class T, eQuaternionLayout Layout, bool Packed>
+auto operator>>(std::basic_istream<Char, CharTraits>& is, Quaternion<T, Layout, Packed>& q) -> std::basic_istream<Char, CharTraits>& {
+	const auto str = impl::ReadArray(is);
+	const auto elements = impl::SplitArray(std::basic_string_view<Char, CharTraits>(str).substr(1, str.size() - 2), Char('@'));
+	if (elements.size() != 2) {
+		throw std::invalid_argument("expected an angle and an axis");
+	}
+	T angle;
+	Vector<T, 3, Packed> axis;
+
+	std::basic_stringstream<Char, CharTraits> ss;
+	ss.str(std::basic_string<Char, CharTraits>(elements[0]));
+	impl::ParseElement(ss, angle);
+	ss.str(std::basic_string<Char, CharTraits>(elements[1]));
+	ss >> axis;
+	const auto theta = Deg2Rad(angle) / T(2);
+	q = Quaternion<T, Layout, Packed>(std::cos(theta), axis * std::sin(theta));
+	return is;
+}
 
 } // namespace mathter
